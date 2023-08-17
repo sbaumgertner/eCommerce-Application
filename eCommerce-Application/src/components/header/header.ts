d85b539e2b85a4369b1f5dab2656615345ffffc6
Illia Sakharau <sakharovia0204@gmail.com>
2023-08-17 20:14:27 +0400

    feat: add isAnonUser var to app-store

--- eCommerce-Application/src/components/header/header.ts
+++ eCommerce-Application/src/components/header/header.ts
@@ -1,5 +1,5 @@
 import './header.scss';
-import { ElementParams, LinkProps, PageName } from '../../types';
+import { ElementParams, LinkProps, PageName, StoreEventType } from '../../types';
 import Component from '../abstract/component';
 import createElement from '../../utils/create-element';
 import htmlToElement from '../../utils/html-to-element';
@@ -11,6 +11,7 @@ import IconWithCounter from '../icon-with-counter/icon-with-counter';
 import darkLogoImg from '../../assets/logo-dark.svg';
 import cartIcon from '../../assets/icons/icon-cart.svg';
 import userIcon from '../../assets/icons/icon-user.svg';
+import { UserTypeAction } from '../../store/action/userTypeAction';
 
 const HeaderNavLinks: LinkProps[] = [
     {
@@ -21,10 +22,16 @@ const HeaderNavLinks: LinkProps[] = [
         page: PageName.ABOUT_US,
         text: 'About us',
     },
+    {
+        page: PageName.ACCOUNT,
+        text: 'Account',
+    },
 ];
 
 export default class Header extends Component {
     private routeAction: RouteAction = new RouteAction();
+    private userAction: UserTypeAction = new UserTypeAction();
+    private isAnonUser: boolean;
 
     constructor(private appStore: AppStore) {
         const headerParams: ElementParams = {
@@ -32,6 +39,13 @@ export default class Header extends Component {
             classes: ['header'],
         };
         super(headerParams);
+        this.appStore.addChangeListener(StoreEventType.USER_TYPE_CHANGE, this.onUserType.bind(this));
+        this.isAnonUser = this.appStore.getIsAnonUser();
+        this.render();
+    }
+
+    protected onUserType(): void {
+        this.isAnonUser = this.appStore.getIsAnonUser();
         this.render();
     }
 
@@ -45,6 +59,7 @@ export default class Header extends Component {
         const navBar = new NavigationBar(this.appStore, HeaderNavLinks, 'dark').getComponent();
         const logoEl = this.createLogo();
         const buttonBarEl = this.createButtonBar();
+
         wrapperEl.append(navBar, logoEl, buttonBarEl);
         return wrapperEl;
     }
@@ -56,36 +71,78 @@ export default class Header extends Component {
     }
 
     private createButtonBar(): HTMLElement {
-        const cartIconInstance = new IconWithCounter(cartIcon, 5);
+        const btnBarEl = createElement({ tag: 'nav', classes: ['header__btn-bar'] });
+        const cartIconEl = this.createCartIcon();
+        const userIconEl = this.createUserIcon();
+        const loginBtnEl = this.createloginButton();
+        const registrationBtnEl = this.createRegistrationButton();
+        const logoutBtn = this.createAccountMenu();
+
+        btnBarEl.append(cartIconEl, userIconEl, loginBtnEl, registrationBtnEl, logoutBtn);
+
+        return btnBarEl;
+    }
+
+    private createCartIcon(): HTMLElement {
+        const cartIconInstance = new IconWithCounter(cartIcon, 0);
         const cartIconEl = cartIconInstance.getComponent();
-        const userIconEl = htmlToElement(`<a class="user-icon">${userIcon}</a>`);
-        const btnBarEl = createElement({
-            tag: 'nav',
-            classes: ['header__btn-bar'],
+
+        // поменять на лиснер изменения в сторе корзины
+        cartIconEl.addEventListener('click', () => {
+            cartIconInstance.setCount(Number(cartIconEl.dataset.count) + 1);
         });
+
+        cartIconEl.addEventListener('click', () =>
+            this.routeAction.changePage({ addHistory: true, page: PageName.CART })
+        );
+
+        return cartIconEl;
+    }
+
+    private createUserIcon(): HTMLElement {
+        const userIconEl = htmlToElement(`<a class="user-icon">${userIcon}</a>`);
+
+        userIconEl.addEventListener('click', () =>
+            this.routeAction.changePage({ addHistory: true, page: PageName.ACCOUNT })
+        );
+        return userIconEl;
+    }
+
+    private createloginButton(): HTMLElement {
         const loginBtnEl = createElement({
             tag: 'div',
             classes: ['button'],
             text: 'Login',
         });
+
+        loginBtnEl.addEventListener('click', () => {
+            this.userAction.changeUserType(false); // удалить после проверки
+            this.routeAction.changePage({ addHistory: true, page: PageName.LOGIN });
+        });
+        return loginBtnEl;
+    }
+
+    private createRegistrationButton(): HTMLElement {
         const registrationBtnEl = createElement({
             tag: 'div',
             classes: ['button'],
             text: 'Registration',
         });
 
-        cartIconEl.addEventListener('click', () => {
-            cartIconInstance.setCount(Number(cartIconEl.dataset.count) + 1);
+        registrationBtnEl.addEventListener('click', () => {
+            this.routeAction.changePage({ addHistory: true, page: PageName.REGISTRATION });
         });
-        loginBtnEl.addEventListener('click', () =>
-            this.routeAction.changePage({ addHistory: true, page: PageName.LOGIN })
-        );
-        registrationBtnEl.addEventListener('click', () =>
-            this.routeAction.changePage({ addHistory: true, page: PageName.REGISTRATION })
-        );
+        return registrationBtnEl;
+    }
 
-        btnBarEl.append(cartIconEl, userIconEl, loginBtnEl, registrationBtnEl);
+    private createAccountMenu(): HTMLElement {
+        const logoutBtnEl = createElement({
+            tag: 'div',
+            classes: ['button'],
+            text: 'Logout',
+        });
 
-        return btnBarEl;
+        logoutBtnEl.addEventListener('click', () => this.userAction.changeUserType(true));
+        return logoutBtnEl;
     }
 }
