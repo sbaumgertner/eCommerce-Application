6e7893b1891ab77223e0f5d2eb497d64bdd3b345
Illia Sakharau <sakharovia0204@gmail.com>
2023-08-17 19:31:26 +0400

    feat: add visiblity count logics to icon with counter

--- eCommerce-Application/src/components/icon-with-counter/icon-with-counter.ts
+++ eCommerce-Application/src/components/icon-with-counter/icon-with-counter.ts
@@ -4,25 +4,27 @@ import Component from '../abstract/component';
 import htmlToElement from '../../utils/html-to-element';
 
 export default class IconWithCounter extends Component {
-    constructor(private icon: string, private count: number) {
+    constructor(private icon: string, count: number) {
         const iconCountParams: ElementParams = {
             tag: 'a',
             classes: ['icon-count'],
         };
         super(iconCountParams);
-        this.render();
+        this.render(count);
     }
 
     public setCount(count: number): void {
-        this.count = count;
-        this.componentElem.dataset.count = `${this.count}`;
+        this.componentElem.dataset.count = `${count}`;
+        if (count > 0) {
+            this.componentElem.classList.add('icon-count_count');
+        }
     }
 
-    public render(): void {
+    public render(count: number): void {
         const iconEl = htmlToElement(`<div class="icon-count__icon">${this.icon}</div>`);
 
         this.componentElem.innerHTML = '';
-        this.setCount(this.count);
+        this.setCount(count);
         this.componentElem.append(iconEl);
     }
 }
