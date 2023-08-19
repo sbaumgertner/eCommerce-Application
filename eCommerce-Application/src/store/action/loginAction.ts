6d8e5e73b31b4d502d8dfe45998462789c8d04fe
Jully13 <7448778@mail.ru>
2023-08-19 14:48:01 +0300

    fix: fix login page styles errors

--- eCommerce-Application/src/store/action/loginAction.ts
+++ eCommerce-Application/src/store/action/loginAction.ts
@@ -1,20 +1,12 @@
 import { ActionType } from '../../types';
-<<<<<<< HEAD
 import { AbstractAction } from '../abstract/action';
-=======
-import { Action } from '../abstract/action';
->>>>>>> b727df6 (feat: add login-store)
 
 export type LoginActionData = {
     email: string;
     password: string;
 };
 
-<<<<<<< HEAD
 export class LoginAction extends AbstractAction {
-=======
-export class LoginAction extends Action {
->>>>>>> b727df6 (feat: add login-store)
     public login(data: LoginActionData): void {
         this.dispatcher.handleAction({ actionType: ActionType.LOGIN, data: JSON.stringify(data) });
     }
