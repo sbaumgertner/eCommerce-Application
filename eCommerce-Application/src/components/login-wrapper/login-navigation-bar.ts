1e8dab8947b9b0b2b9fc261c0aad8125a071489b
Jully13 <7448778@mail.ru>
2023-08-20 22:07:05 +0300

    fix: fix login navigation bar error

--- eCommerce-Application/src/components/login-wrapper/login-navigation-bar.ts
+++ eCommerce-Application/src/components/login-wrapper/login-navigation-bar.ts
@@ -1,21 +1,17 @@
 import { AppStore } from '../../store/app-store';
-import { LinkProps, PageName } from '../../types';
+import { PageName } from '../../types';
 import NavigationBar from '../navigation-bar/navigation-bar';
-
-const NavLinks: LinkProps[] = [
-    {
-        page: PageName.LOGIN,
-        text: 'Login',
-    },
-    {
-        page: PageName.REGISTRATION,
-        text: 'Registration',
-    },
-];
 
 export default class LoginNavigationBar extends NavigationBar {
     constructor(appStore: AppStore) {
-        super(appStore, NavLinks, 'dark');
+        super(
+            appStore,
+            [
+                { page: PageName.LOGIN, text: PageName.LOGIN },
+                { page: PageName.REGISTRATION, text: PageName.REGISTRATION },
+            ],
+            'dark'
+        );
     }
 
     protected init(): void {
