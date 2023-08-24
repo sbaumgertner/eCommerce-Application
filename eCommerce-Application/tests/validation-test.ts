c2eea16b0a89b835e8effea5aeab6596a0bef5d0
Jully13 <7448778@mail.ru>
2023-08-24 15:55:16 +0300

    feat: add email and password validation tests

--- eCommerce-Application/tests/validation-test.ts
+++ eCommerce-Application/tests/validation-test.ts
@@ -4,3 +4,13 @@ test('Validation.checkText', () => {
   expect(Validation.checkText('AaПр').isValid).toBe(true);
   expect(Validation.checkText('Aa2').isValid).toBe(false);
 });
+
+test('Validation.checkEmail', () => {
+  expect(Validation.checkEmail('test@test.com').isValid).toBe(true);
+  expect(Validation.checkEmail('test @test. com').isValid).toBe(false);
+});
+
+test('Validation.checkPassword', () => {
+  expect(Validation.checkPassword('Test123!').isValid).toBe(true);
+  expect(Validation.checkPassword('12345').isValid).toBe(false);
+});
