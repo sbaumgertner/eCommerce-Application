78d82bead578742ae0f733b0ec17acec2067e5e1
Illia Sakharau <sakharovia0204@gmail.com>
2023-09-14 21:55:36 +0400

    feat: implement promocode section

--- eCommerce-Application/src/pages/home/home.ts
+++ eCommerce-Application/src/pages/home/home.ts
@@ -7,6 +7,32 @@ import createElement from '../../utils/create-element';
 import { Page } from '../abstract/page';
 import { RouteAction } from '../../store/action/routeAction';
 
+type PromocodeInfo = {
+    name: string;
+    code: string;
+    description: string;
+};
+
+const PROMO_CODES_INFO: PromocodeInfo[] = [
+    {
+        name: 'Discount 50%',
+        code: 'PROMO-50',
+        description: `Products without discount
+        Any total amount`,
+    },
+    {
+        name: 'Discount 30%',
+        code: 'PROMO-30',
+        description: `All production
+        Any total amount`,
+    },
+    {
+        name: 'Discount $45',
+        code: 'PROMO-45USD',
+        description: `All production
+        Cart total amount from $100`,
+    },
+];
 const ImplimentedPages: LinkProps[] = [
     {
         page: PageName.INDEX,
@@ -51,7 +77,7 @@ export class HomePage extends Page {
 
     public render(): void {
         this.html = document.createElement('div');
-        this.html.append(this.createHeroBannerSection(), this.createAllLinksSection());
+        this.html.append(this.createHeroBannerSection(), this.createAllLinksSection(), this.createPromocodeSection());
     }
 
     private createHeroBannerSection(): HTMLElement {
@@ -90,5 +116,56 @@ export class HomePage extends Page {
         wrapperEl.append(titleImplemented, textImplemented, this.implimentedPages);
         allLinksEl.append(wrapperEl);
         return allLinksEl;
+    }
+
+    private createPromocodeSection(): HTMLElement {
+        const sectionEl = createElement({ tag: 'section', classes: ['promo-code'] });
+        const wrapperEl = createElement({ tag: 'div', classes: ['wrapper', 'promo-code__wrapper'] });
+        const titleImplemented = createElement({
+            tag: 'h3',
+            classes: ['promo-code__title'],
+            text: 'Promo Codes',
+        });
+        const listEl = createElement({ tag: 'div', classes: ['promo-code__list'] });
+
+        PROMO_CODES_INFO.forEach((promoInfo) => {
+            const promoCardEl = this.createPromocodeCard(promoInfo);
+            listEl.append(promoCardEl);
+        });
+        wrapperEl.append(titleImplemented, listEl);
+        sectionEl.append(wrapperEl);
+        return sectionEl;
+    }
+
+    private createPromocodeCard(promoInfo: PromocodeInfo): HTMLElement {
+        const { name, code, description } = promoInfo;
+        const cardEl = createElement({ tag: 'div', classes: ['promo-card'] });
+        const headerEl = createElement({ tag: 'div', classes: ['promo-card__header'] });
+        const headerBlockEl = createElement({ tag: 'div', classes: ['promo-card__header-block'] });
+        const titleEl = createElement({ tag: 'h5', classes: ['promo-card__title'], text: name });
+        const codeEl = createElement({ tag: 'h6', classes: ['promo-card__code'], text: code });
+        const btnEl = new Button('bordered', undefined, 'Copy code').getComponent();
+        const descriptionEl = createElement({ tag: 'ul', classes: ['promo-card__description'] });
+
+        btnEl.addEventListener('click', () => {
+            cardEl.classList.remove('promo-card_copied');
+            navigator.clipboard
+                .writeText(code)
+                .then(() => {
+                    // window.alert(`Code "${code}" copied!`);
+                    cardEl.classList.add('promo-card_copied');
+                })
+                .catch(() => {
+                    window.alert('Something went wrong :(');
+                });
+        });
+        description.split('\n').forEach((li) => {
+            const liEl = createElement({ tag: 'li', classes: ['promo-card__li'], text: li });
+            descriptionEl.append(liEl);
+        });
+        headerBlockEl.append(titleEl, codeEl);
+        headerEl.append(headerBlockEl, btnEl);
+        cardEl.append(headerEl, descriptionEl);
+        return cardEl;
     }
 }
