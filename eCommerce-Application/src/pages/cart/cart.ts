09c6ad10999dba578c01bc547fd3080f52c342d3
Illia Sakharau <sakharovia0204@gmail.com>
2023-09-15 10:31:27 +0400

    feat: implement empty cart view

--- eCommerce-Application/src/pages/cart/cart.ts
+++ eCommerce-Application/src/pages/cart/cart.ts
@@ -6,12 +6,14 @@ import { CartActions } from '../../store/action/cartActions';
 import { Button } from '../../components/button/button';
 import { CartStore } from '../../store/cart-store';
 
+const EMPTY_CART_TITLE = `Your home is missing something... a little green!`;
+const EMPTY_CART_MESSAGE = `Take a look at our curated plant collections to find the perfect addition to your space.`;
+
 export class CartPage extends Page {
-    private cartAction: CartActions;
+    private cartAction = new CartActions();
 
     constructor(private appStore: AppStore, private cartStore: CartStore) {
         super();
-        this.cartAction = new CartActions();
     }
 
     public render(): void {
@@ -20,22 +22,32 @@ export class CartPage extends Page {
     }
 
     private createCartSection(): HTMLElement {
-        const heroBannerEl = createElement({ tag: 'section', classes: ['cart'] });
+        const sectionEl = createElement({ tag: 'section', classes: ['cart'] });
         const wrapperEl = createElement({ tag: 'div', classes: ['wrapper', 'cart__wrapper'] });
         const myCartEl = this.createMyCart();
         const summaryEl = this.createSummary();
 
-        wrapperEl.append(myCartEl, summaryEl);
-        heroBannerEl.append(wrapperEl);
-        return heroBannerEl;
+        if (this.cartStore.getCartItemAmount() === 0) {
+            wrapperEl.append(myCartEl);
+        } else {
+            wrapperEl.append(myCartEl, summaryEl);
+        }
+        sectionEl.append(wrapperEl);
+        return sectionEl;
     }
 
     private createMyCart(): HTMLElement {
         const myCartEl = createElement({ tag: 'div', classes: ['cart__my-cart'] });
         const headerEl = this.createMyCartHeader();
         const listEl = this.createMyCartList();
+        const emptyEl = this.createMyCartEmpty();
 
-        myCartEl.append(headerEl, listEl);
+        if (this.cartStore.getCartItemAmount() === 0) {
+            myCartEl.classList.add('cart__my-cart_empty');
+            myCartEl.append(headerEl, emptyEl);
+        } else {
+            myCartEl.append(headerEl, listEl);
+        }
         return myCartEl;
     }
 
@@ -46,7 +58,12 @@ export class CartPage extends Page {
             classes: ['my-cart__title'],
             text: 'My Cart',
         });
-        const btnResetCartEl = new Button('bordered', 'clear-cart', 'Clear cart').getComponent();
+        const btnResetCart = new Button('bordered', 'clear-cart', 'Clear cart');
+        const btnResetCartEl = btnResetCart.getComponent();
+
+        if (this.cartStore.getCartItemAmount() === 0) {
+            btnResetCart.disable();
+        }
         btnResetCartEl.classList.add('button_bordered_negative');
         btnResetCartEl.addEventListener('click', () => {
             this.cartAction.clearCart();
@@ -64,6 +81,16 @@ export class CartPage extends Page {
             text: 'CART_LIST',
         });
 
+        return cartListEl;
+    }
+
+    private createMyCartEmpty(): HTMLElement {
+        const cartListEl = createElement({ tag: 'div', classes: ['empty-cart'] });
+        const titleEl = createElement({ tag: 'h2', classes: ['empty-cart__title'], text: EMPTY_CART_TITLE });
+        const messageEl = createElement({ tag: 'h5', classes: ['empty-cart__message'], text: EMPTY_CART_MESSAGE });
+        const btnEl = new Button('filled', '', 'Go to Catalog').getComponent();
+
+        cartListEl.append(titleEl, messageEl, btnEl);
         return cartListEl;
     }
 
